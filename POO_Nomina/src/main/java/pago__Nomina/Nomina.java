e39eddd1cd3860c0db0a5ea9edc1aa0a352d4815
mi-ley <147452629+mi-ley@users.noreply.github.com>
2024-06-19 14:22:08 -0600

    pago de nomina se creo la clase sin funcionalidad

---
+++ POO_Nomina/src/main/java/pago__Nomina/Nomina.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pago__Nomina;
+
+/**
+ *
+ * @author ernes
+ */
+/*public abstract class Nomina {
+    private 
+    
+}*/
